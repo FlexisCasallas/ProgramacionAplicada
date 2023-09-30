ac576114669c7097ea3c7ccfdb2fd2806f11c90b
FlexisCasallas <142937590+FlexisCasallas@users.noreply.github.com>
2023-09-30 14:20:09 -0500

    TercerPrograma.py

---
+++ PrimerosProgramas/TercerPrograma.py
@@ -0,0 +1,19 @@
+meal = "An english muffin"
+
+# Printing out breakfast
+print("Breakfast:")
+print(meal)
+
+# Now update meal to be lunch!
+meal = "A colombian recipe"
+
+# Printing out lunch
+print("Lunch:")
+print(meal)
+
+
+meal = "Cerealitos"
+
+# Printing out dinner
+print("Dinner:")
+print(meal)
