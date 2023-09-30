e8dd451793ef92d21faeb2f6d3e4711ee2bab783
FlexisCasallas <142937590+FlexisCasallas@users.noreply.github.com>
2023-09-30 14:33:05 -0500

    FuncionFactorial.py

---
+++ Funciones/FuncionFactorial.py
@@ -0,0 +1,12 @@
+def factorial(a):
+    resultado=1
+    if a>1:
+      for i in range(1, a+1):
+         resultado=resultado*i
+    print('el resultado es: ', resultado)
+    return resultado
+
+a=int(input('Ingrese el numero: '))
+print('El numero es: ', a)
+valor=factorial(a)
+print(valor)
